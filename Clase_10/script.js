84f9704ee6adee00d6815ece0ed11a3c0460a13d
prietosebastian-dev <sebaprieto1007@gmail.com>
2023-12-20 15:17:30 -0300

    refresh

---
+++ Clase_10/script.js
@@ -0,0 +1,26 @@
+/*Manejamos datos-informacion
+Tipos de valores interpretados por JS: */
+
+// STRING (caracteres) -> Comillas "" `` ''
+"Juan" //string
+'Admin123' //string
+`Hola` //string
+
+// Numbers -> Numeros
+123
+123.45 //Numero decimal
+
+// booleans (booleanos)
+//verdadero y falso -> Palabras asignadas por JS
+true
+false
+
+// Almacenar informacion -> Variables (espacio de memoria donde guardamos info que utilizamos a lo largo de todo mi documento)
+
+// crar variable
+//utilizar variable
+//modificar variable
+
+// Crear variable -> Palabra Reservada
+
+//pal nombre= valor;
